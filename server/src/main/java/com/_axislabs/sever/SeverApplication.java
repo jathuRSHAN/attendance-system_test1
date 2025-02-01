818b20e8bc7b4e71e746c30f07475f3aea9fc04e
jathuRSHAN <sjathurshan19980419@gmail.com>
2025-02-02 00:27:13 +0530

    #2, initialized Spring Boot server

---
+++ server/src/main/java/com/_axislabs/sever/SeverApplication.java
@@ -0,0 +1,13 @@
+package com._axislabs.sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverApplication.class, args);
+	}
+
+}
